5a90358cf5c4eca547875cee5677284739259645
Kalash Thakur <83134042+KalashThakur@users.noreply.github.com>
2022-08-24 22:54:43 +0530

    Navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Footer from '../components/Footer/Footer'
+import Navbar from '../components/HomePage/Navbar'
 
 const Home = () => {
   return (
     <div>
       {/* <h1>home</h1> */}
+      <Navbar />
       <Footer />
     </div>
   )
