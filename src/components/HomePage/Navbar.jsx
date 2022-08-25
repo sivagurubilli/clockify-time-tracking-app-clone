e4c5e69e7aa397aa084990003783f5f087086193
Manya7864 <manishyadav78642080@gmail.com>
2022-08-25 23:27:38 +0530

    day2

--- src/components/HomePage/Navbar.jsx
+++ src/components/HomePage/Navbar.jsx
@@ -4,6 +4,7 @@ import {
     Button
 } from '@chakra-ui/react';
 import style from "./Navbar.module.css"
+import { Link } from 'react-router-dom';
 
  const Navbar = () => {
   return (
@@ -22,12 +23,17 @@ import style from "./Navbar.module.css"
         {/* Account */}
         <div className={style.acc}>
             <Box>
+            <Link to="/login">
                 <button className={style.btn1} colorScheme='teal' variant='ghost'>LOG IN</button>
+                </Link>
             </Box>
             <Box>
-                <button className={style.btn}>
+            <Link to="/signup">          
+               <button className={style.btn}>
                 SIGN UP FREE
                 </button>
+                </Link>
+   
             </Box>
         </div>
     </nav>
