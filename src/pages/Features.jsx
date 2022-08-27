a8f795b3d35cf0d39fb5cfcf09d47432127ce3cf
Mayank Dhyani <mayankdhyani.mbd@gmail.com>
2022-08-27 15:42:41 +0530

    done

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -1,15 +1,34 @@
 import React from "react";
-import a from "./featuresimage/Screenshot (32).png"
+import a from "./featuresimage/Screenshot (32).png";
+import b from "./featuresimage/Screenshot (33).png";
+import c from "./featuresimage/Screenshot (34).png";
+import d from "./featuresimage/Screenshot (35).png";
+import e from "./featuresimage/Screenshot (36).png";
+import f from "./featuresimage/Screenshot (37).png";
+import g from "./featuresimage/Screenshot (38).png";
+import h from "./featuresimage/Screenshot (39).png";
+import i from "./featuresimage/Screenshot (40).png";
+import j from "./featuresimage/Screenshot (41).png";
 
-const featuresPage = ()=>{
+
+const Featurespage = ()=>{
     
     return(
         <div>
-            <div className="Upper">
-                <img src="" alt="" />
+            <div style={{gap:"20px"}} className="Upper">
+                <img src={a} alt="" />
+                <img src={b} alt="" />
+                <img src={c} alt="" />
+                <img src={d} alt="" />
+                <img src={e} alt="" />
+                <img src={f} alt="" />
+                <img src={g} alt="" />
+                <img src={h} alt="" />
+                <img src={i} alt="" />
+                <img src={j} alt="" />
             </div>
         </div>
     )
 }
 
-export default featuresPage;
+export default Featurespage;
