6ca43df561b875b8fad3c7230613597f839c07ec
Mayank Dhyani <mayankdhyani.mbd@gmail.com>
2022-08-27 14:09:14 +0530

    push

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -1,1 +1,15 @@
 import React from "react";
+import a from "./featuresimage/Screenshot (32).png"
+
+const featuresPage = ()=>{
+    
+    return(
+        <div>
+            <div className="Upper">
+                <img src="" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default featuresPage;
