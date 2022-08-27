aa4bce128f4966e1338a616eef96b94079f65d60
Mayank Dhyani <mayankdhyani.mbd@gmail.com>
2022-08-27 15:58:56 +0530

    done

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -1,3 +1,4 @@
+import { Center } from "@chakra-ui/react";
 import React from "react";
 import a from "./featuresimage/Screenshot (32).png";
 import b from "./featuresimage/Screenshot (33).png";
@@ -9,23 +10,26 @@ import g from "./featuresimage/Screenshot (38).png";
 import h from "./featuresimage/Screenshot (39).png";
 import i from "./featuresimage/Screenshot (40).png";
 import j from "./featuresimage/Screenshot (41).png";
+import Footer from "../components/Footer/Footer";
 
 
 const Featurespage = ()=>{
     
     return(
         <div>
-            <div style={{gap:"20px"}} className="Upper">
-                <img src={a} alt="" />
-                <img src={b} alt="" />
-                <img src={c} alt="" />
-                <img src={d} alt="" />
-                <img src={e} alt="" />
-                <img src={f} alt="" />
-                <img src={g} alt="" />
-                <img src={h} alt="" />
-                <img src={i} alt="" />
-                <img src={j} alt="" />
+            <div className="Upper">
+                <img style={{padding:"10px"}} src={a} alt="" />
+                <img style={{padding:"10px"}} src={b} alt="" />
+                <img style={{padding:"10px"}} src={c} alt="" />
+                <img style={{padding:"10px"}} src={d} alt="" />
+                <h1 style={{textAlign:"center",fontWeight:"bolder",fontSize:30}}>Calendar</h1>
+                <h1 style={{textAlign:"center", fontSize:30,color:"grey"}}>Visualize your time and block out activities.</h1>
+                <img style={{padding:"10px"}} src={e} alt="" />
+                <img style={{padding:"10px"}} src={f} alt="" />
+                <img style={{padding:"10px"}} src={g} alt="" />
+                <img style={{padding:"10px"}} src={h} alt="" />
+                <img style={{padding:"10px"}} src={i} alt="" />
+                <Footer/>
             </div>
         </div>
     )
