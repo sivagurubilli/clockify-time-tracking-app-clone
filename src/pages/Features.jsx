d67ceeb4dd29ef41e7bff053174c81fa22d6d713
Mayank Dhyani <mayankdhyani.mbd@gmail.com>
2022-08-27 19:21:55 +0530

    push

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -9,7 +9,7 @@ import f from "./featuresimage/Screenshot (37).png";
 import g from "./featuresimage/Screenshot (38).png";
 import h from "./featuresimage/Screenshot (39).png";
 import i from "./featuresimage/Screenshot (40).png";
-import j from "./featuresimage/Screenshot (41).png";
+import k from "./featuresimage/Screenshot (42).png";
 import Footer from "../components/Footer/Footer";
 import Navbar from "./timetracker/navbar/Navbar";
 
@@ -25,11 +25,18 @@ const Featurespage = ()=>{
                 <img style={{padding:"10px"}} src={c} alt="" />
                 <img style={{padding:"10px"}} src={d} alt="" />
                 <h1 style={{textAlign:"center",fontWeight:"bolder",fontSize:30}}>Calendar</h1>
-                <h1 style={{textAlign:"center", fontSize:30,color:"grey"}}>Visualize your time and block out activities.</h1>
+                <h3 style={{textAlign:"center",color:"grey"}}>Visualize your time and block out activities.</h3>
                 <img style={{padding:"10px"}} src={e} alt="" />
+                <h1 style={{textAlign:"center",fontWeight:"bolder",fontSize:30}}>Kiosk</h1>
+                <h3 style={{textAlign:"center",color:"grey"}}>Clock in with your PIN code from a shared device.</h3>
+                <img style={{padding:"10px"}} src={k} alt="" />
+                <h1 style={{textAlign:"center",fontWeight:"bolder",fontSize:30}}>Apps</h1>
+                <h3 style={{textAlign:"center",color:"grey"}}>Track time using desktop and mobile app.</h3>
                 <img style={{padding:"10px"}} src={f} alt="" />
                 <img style={{padding:"10px"}} src={g} alt="" />
                 <img style={{padding:"10px"}} src={h} alt="" />
+                <h1 style={{textAlign:"center",fontWeight:"bolder",fontSize:30}}>Reports</h1>
+                <h3 style={{textAlign:"center",color:"grey"}}>See who worked on what, how much money you earn, review your team's time, and export the data.</h3>
                 <img style={{padding:"10px"}} src={i} alt="" />
                 <Footer/>
             </div>
